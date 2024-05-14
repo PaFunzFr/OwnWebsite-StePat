c9cb073962501325bd88f1cf226ffa57bfab613c
funZ <153361103+PaFunzFr@users.noreply.github.com>
2024-05-14 17:03:08 +0200

    Add files via upload

--- 01-resources/12-js/main.js
+++ 01-resources/12-js/main.js
@@ -20,3 +20,20 @@ buttonPrevious.addEventListener('click', () => {
         slider.scrollTo({ left: slider.scrollWidth - sliderVisibleWidth, behavior: 'smooth' });
     }
 });
+
+
+const hoveredItems = document.querySelectorAll('.report-item, .photos-item');
+
+hoveredItems.forEach(item => {
+    item.addEventListener('mouseover', () => {
+        const highlightedTexts = event.currentTarget.querySelector('.report-title, .photos-title');
+        highlightedTexts.style.color = 'hsla(37, 65%, 62%, 1)';
+        highlightedTexts.style.fontWeight = 'bolder';
+    });
+
+    item.addEventListener('mouseout', () => {
+        const highlightedTexts = event.currentTarget.querySelector('.report-title, .photos-title');
+        highlightedTexts.style.color = '';
+        highlightedTexts.style.fontWeight = '';
+    });
+});
